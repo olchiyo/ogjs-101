fb30d775a12a41f47ac357960ad1f0cb04f489e0
prudentcircle <prudentcircle@outlook.com>
2023-07-05 21:47:12 +0900

    fix passwd problem

--- Scene03/o101s03-alone.sh
+++ Scene03/o101s03-alone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'root' | passwd --stdin root
-apt-get update
+echo -e 'root\nroot' | passwd root
+apt-get -y update
 apt-get install -y openssh-server
 sed -i '/^PermitRootLogin/d' /etc/ssh/sshd_config
 bash -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config'
