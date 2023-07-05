1f84fb3228ad9cbdf2c66d3132781c19877af57a
prudentcircle <prudentcircle@outlook.com>
2023-07-05 21:41:49 +0900

    fix STDIN problem

--- Scene03/o101s03-alone.sh
+++ Scene03/o101s03-alone.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-echo 'root' | passwd root
+echo 'root' | passwd --stdin root
 apt-get update
-apt-get install openssh-server 
 apt-get install -y openssh-server
 sed -i '/^PermitRootLogin/d' /etc/ssh/sshd_config
 bash -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config'
-systemctl restart ssh
+systemctl restart sshcat 
