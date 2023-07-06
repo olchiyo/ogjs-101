614bfdddc126a24e5764b2e2ff28fdd1aea41882
prudentcircle <prudentcircle@outlook.com>
2023-07-07 00:35:13 +0900

    fix errors

--- Scene03/o101s03_alone_start.sh
+++ Scene03/o101s03_alone_start.sh
@@ -65,7 +65,7 @@ then
     echo "o101s03-alone VM already exists. DELETING..."
     lxc stop o101s03-alone --force && sleep 2
     lxc delete o101s03-alone --force && sleep 2
-    lxc launch images:debian/bookworm/default o101s03-alone --vm --network o101-net --storage default
+    lxc launch 'images:debian/bookworm/default' o101s03-alone --vm --network o101-net --storage default
     sleep 5
     lxc list | grep o101s03-alone
     if [ $? -eq 0 ];
