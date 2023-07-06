2e97a4d0e2061ed100bbab35c96180f63b0a0575
prudentcircle <prudentcircle@outlook.com>
2023-07-06 21:52:45 +0900

    improve functions

--- Scene03/o101s03_alone_prov.sh
+++ Scene03/o101s03_alone_prov.sh
@@ -17,10 +17,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 echo -e 'root\nroot' | passwd root
 apt-get -y update
 apt-get install -y openssh-server
 sed -i '/^PermitRootLogin/d' /etc/ssh/sshd_config
 bash -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config'
 systemctl restart ssh
+
+parted --script /dev/sdb \
+    mklabel msdos \
+    mkpart primary ext4 1MiB 300MiB \
+    mkpart primary xfs 300MiB 800MiB
+
+parted --script /dev/sdc \
+    mklabel msdos \
+    mkpart primary ext3 0% 20% \
+    mkpart primary xfs 20% 60%
+
+mkfs.ext4 /dev/sdb1
+mkfs.xfs /dev/sdb2
+mkfs.ext3 /dev/sdc1
+
+mkdir -p /mnt/ongojishin
+mkdir -p /opt/austin
+mkdir -p /svr/newjeans
+
+
+mount /dev/sdb1 /mnt/ongojishin
+mount /dev/sdb2 /opt/austin
+mount /dev/sdc1 /srv/newjeans
--- Scene03/o101s03_alone_start.sh
+++ Scene03/o101s03_alone_start.sh
@@ -17,6 +17,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+while [ -z $DNAT_IP ]; do
+    echo "Enter your DNAT_IP:"
+    read DNAT_IP
+done
+
+if [[ $DNAT_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "success"
+else
+  echo "ERROR:: Wrong IP Address"
+  exit 1
+fi
+
+
+
 lxc network list | grep o101-net
 if [ $? -eq 0 ] 
 then 
@@ -31,7 +45,7 @@ if [ $? -eq 0 ]
 then 
     echo "It is confirmed that Storage "default" exists"
     lxc storage list | grep CREATED
-    if [$? -eq 0 ]
+    if [ $? -eq 0 ]
     then
         echo -e "It is confirmed that Storage "default" is in a running state"
     else
@@ -47,18 +61,30 @@ lxc list | grep o101s03-alone
 if [ $? -eq 0 ]
 then
     echo "o101s03-alone VM already exists. DELETING..."
-    lxc stop o101s03-alone --force && lxc delete o101s03 --force
-else
-    echo "o101s03-alone is not present. CREATING..."
+    lxc stop o101s03-alone --force
+    lxc delete o101s03-alone --force
     lxc launch images:debian/bookworm/default o101s03-alone --vm --network o101-net --storage default
-    lxc list | grep o101s03-again
-    
+    sleep 5
+    lxc list | grep o101s03-alone
     if [ $? -eq 0 ]
     then
         echo -e "o101s03-alone successfully created."
     else
         echo "ERROR:: o101s03-alone not created. EXITING..."
         exit 1
+    fi
+else
+    echo "o101s03-alone is not present. CREATING..."
+    lxc launch images:debian/bookworm/default o101s03-alone --vm --network o101-net --storage default
+    sleep 5
+    lxc list | grep o101s03-alone
+    if [ $? -eq 0 ]
+    then
+        echo -e "o101s03-alone successfully created."
+    else
+        echo "ERROR:: o101s03-alone not created. EXITING..."
+        exit 1
+    fi
 fi
 
 lxc storage volume list default | grep o101s03-alone-vol01
@@ -93,11 +119,32 @@ else
     echo "o101s03-alone-vol02 is not present. CREATING..."
     lxc storage volume create default o101s03-alone-vol02 --type=block size=1073741824
     echo "Attaching o101s03-alone-vol02 to o101s03-alone..."
-    lxc config device add o101s03-alone /dev/sdc disk pool=default source=o101s03-alone-vol02 
+    lxc config device add o101s03-alone /dev/sdc disk pool=default source=o101s03-alone-vol02
 fi
 
+lxc exec o101s03-alone -- ls || EXIT_CODE=$?
+while ! lxc exec o101s03-alone -- ls
+do
+    sleep 2
+done
 
+echo -e "Now, provisioning o101s03-alone"
 
+lxc exec o101s03-alone -- bash -c "echo 'deb http://ftp.kr.debian.org/debian bookworm main' > /etc/apt/sources.list.d/korea.list && echo 'deb http://ftp.kr.debian.org/debian bookworm-updates main' >> /etc/apt/sources.list.d/korea.list && echo 'deb http://ftp.kr.debian.org/debian-security/ bookworm-security main' >> /etc/apt/sources.list.d/korea.list && sed -i 's/^\([^#]\)/#\1/g' /etc/apt/sources.list"
+lxc exec o101s03-alone -- bash -c "apt-get update -y && apt-get install -y curl"
+lxc exec o101s03-alone -- bash -c "curl -fsSL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/olchiyo/ogjs-101/main/Scene03/o101s03_alone_prov.sh | bash"
 
+VM_IP=$(lxc list | grep o101s03-alone | awk '{print $6}')
 
+lxc network forward list o101-net | grep $DNAT_IP
+if [ $? -eq 1 ]
+then
+    lxc network forward create o101-net $DNAT_IP
+    lxc network forward port add o101-net $DNAT_IP tcp 10022 $VM_IP 22
+else
+    lxc network forward port add o101-net $DNAT_IP tcp 10022 $VM_IP 22
+fi
+
+echo "Enter the following Command to connect to o101s03-alone --"
+printf 'ssh -p 10022 root@%s\n' "$DNAT_IP"
 
