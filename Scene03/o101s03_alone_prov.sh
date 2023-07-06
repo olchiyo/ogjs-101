3d653983315b5dffd3420f73313748a2a5fe00e2
prudentcircle <prudentcircle@outlook.com>
2023-07-06 23:27:04 +0900

    fix errors

--- Scene03/o101s03_alone_prov.sh
+++ Scene03/o101s03_alone_prov.sh
@@ -23,6 +23,22 @@ apt-get install -y openssh-server parted xfsprogs
 sed -i '/^PermitRootLogin/d' /etc/ssh/sshd_config
 bash -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config'
 systemctl restart ssh
+cat << EOF > /etc/locale.conf
+LANG=en_US.UTF-8
+LC_CTYPE=en_US.UTF-8 
+LC_NUMERIC=en_US.UTF-8
+LC_TIME=en_US.UTF-8
+LC_COLLATE=en_US.UTF-8
+LC_MONETARY=en_US.UTF-8
+LC_MESSAGES=en_US.UTF-8
+LC_PAPER=en_US.UTF-8
+LC_NAME=en_US.UTF-8
+LC_ADDRESS=en_US.UTF-8
+LC_TELEPHONE=en_US.UTF-8
+LC_MEASUREMENT=en_US.UTF-8
+LC_IDENTIFICATION=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+EOF
 
 parted --script /dev/sdb \
     mklabel msdos \
@@ -40,7 +56,7 @@ mkfs.ext3 /dev/sdc1
 
 mkdir -p /mnt/ongojishin
 mkdir -p /opt/austin
-mkdir -p /svr/newjeans
+mkdir -p /srv/newjeans
 
 
 mount /dev/sdb1 /mnt/ongojishin
