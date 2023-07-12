266ebe25dd12ed0a4b22414223d0f83062aadd2f
prudentcircle <prudentcircle@outlook.com>
2023-07-12 16:23:13 +0900

    fix errors

--- Scene03/o101s03_alone_finish.sh
+++ Scene03/o101s03_alone_finish.sh
@@ -17,7 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DNAT_IP=""
 INSTANCE="o101s03-alone"
 NETWORK="o101-net"
 STORAGE="default"
